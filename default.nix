4f108f6d1e3f44e3480a231f40632f73ddf96129
Andrei Volt <andrei@avolt.net>
2024-02-06 12:28:14 +0100

    package for Nix

--- default.nix
+++ default.nix
@@ -1,19 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.python3Packages.buildPythonPackage rec {
+with pkgs;
+
+python3Packages.buildPythonPackage rec {
   pname = "scihub";
   version = "1.0";
 
   src = ./.;
 
-  propagatedBuildInputs = with pkgs.python3Packages; [
+  propagatedBuildInputs = with python3Packages; [
     beautifulsoup4
     requests
     retrying
     pysocks
   ];
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "Python API and command-line tool for Sci-Hub";
     homepage = "https://github.com/zaytoun/scihub.py/";
     license = licenses.mit;
